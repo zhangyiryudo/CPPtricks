7ea8ff529dfce7c651e169c69e1315a84ee464c5
luis.zhang.ca <luis.zhang.ca@gmail.com>
2026-04-26 15:08:21 +0000

    SsoSimple

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include "std_function.h"
 #include "parse_FIX_zero_copy.h"
 #include "string_SSO.h"
-
+using namespace std;
 
 int main() {
-    test_std_function();
-    printParsedFIXFields();
-    store_lambda_in_vector();
+    // test_std_function();
+    // printParsedFIXFields();
+    // store_lambda_in_vector();
 
     std::cout << sizeof(std::string) << std::endl; // 32 on my system, due to SSO optimization
     std::cout << sizeof(bool) << std::endl; // 1 on my system
@@ -18,5 +18,13 @@ int main() {
     std::cout << "s1: " << s1.c_str() << " (size: " << s1.size() << ", is_local: " << s1.is_local() << ")" << std::endl;
     std::cout << "s2: " << s2.c_str() << " (size: " << s2.size() << ", is_local: " << s2.is_local() << ")" << std::endl;
 
+
+    SsoSimpleString s3("Short string"); // Should use short buffer
+    SsoSimpleString s4("This is a longer string that will use the long buffer"); // Should use long buffer
+    std::cout << "s3: " << s3.c_str() << " (size: " << s3.size() << ", is_long: " << s3.is_long_string() << ")" << std::endl;
+    std::cout << "s4: " << s4.c_str() << " (size: " << s4.size() << ", is_long: " << s4.is_long_string() << ")" << std::endl;
+
+    cout << "sizeof(SSOString): " << sizeof(SSOString) << endl; // 24 bytes due to SSO optimization
+    cout << "sizeof(SsoSimpleString): " << sizeof(SsoSimpleString) << endl; // 32 bytes
     return 0;
 }
--- string_SSO.h
+++ string_SSO.h
@@ -22,7 +22,7 @@ private:
         // Layout 2: Used when the string is short (stored locally)
         struct {
             char buffer[SSO_CAPACITY];
-            unsigned char length_and_flag; 
+            unsigned char length_and_flag;// sizeof(unsigned char) is 1 byte
         } local;
     } data;
 
@@ -67,5 +67,85 @@ public:
 
     const char* c_str() const {
         return is_local() ? data.local.buffer : data.heap.ptr;
+    }
+};
+
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+class SsoSimpleString {
+private:
+    static constexpr size_t MAX_SHORT = 23;
+    bool is_long {false};//sizeof(bool) is 1 byte
+
+    union {
+        struct {
+            char* data;// sizeof(char*) is 8 bytes on a 64-bit system
+            size_t size;// sizeof(size_t) is 8 bytes on a 64-bit system
+            size_t capacity;// sizeof(size_t) is 8 bytes on a 64-bit system
+        } l;
+        char short_buf[MAX_SHORT + 1];// +1 for null terminator, total 24 bytes
+    };
+
+public:
+    bool is_long_string() const {
+        return is_long;
+    }
+    // Constructor
+    SsoSimpleString(const char* s = "") {
+        size_t len = std::strlen(s);
+        if (len <= MAX_SHORT) {
+            is_long = false;
+            std::memcpy(short_buf, s, len + 1);
+            short_buf[len] = '\0'; // Ensure null termination
+        } else {
+            is_long = true;
+            l.size = len;
+            l.capacity = len;
+            l.data = new char[l.capacity + 1];
+            std::memcpy(l.data, s, len + 1);
+        }
+    }
+
+    // Destructor
+    ~SsoSimpleString() {
+        if (is_long) delete[] l.data;
+    }
+
+    // Copy Constructor
+    SsoSimpleString(const SsoSimpleString& other) : is_long(other.is_long) {
+        if (is_long) {
+            l.size = other.l.size;
+            l.capacity = other.l.capacity;
+            l.data = new char[l.capacity + 1];
+            std::memcpy(l.data, other.l.data, l.size + 1);
+        } else {
+            std::memcpy(short_buf, other.short_buf, MAX_SHORT + 1);
+        }
+    }
+
+    // Move Constructor
+    SsoSimpleString(SsoSimpleString&& other) noexcept : is_long(other.is_long) {
+        if (is_long) {
+            l = other.l;
+            other.l.data = nullptr; // "Steal" the pointer
+        } else {
+            std::memcpy(short_buf, other.short_buf, MAX_SHORT + 1);
+        }
+        other.is_long = false; // Reset other to an empty short string
+    }
+
+    const char* c_str() const {
+        return is_long ? l.data : short_buf;
+    }
+
+    size_t size() const {
+        return is_long ? l.size : std::strlen(short_buf);
+    }
+
+    size_t capacity() const {
+        return is_long ? l.capacity : MAX_SHORT;
     }
 };
